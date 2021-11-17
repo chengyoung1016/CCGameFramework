25d4b5af3b796e26c92e0ab20dda1a590b8d886b
chengyoung1016 <93090261@qq.com>
2021-11-17 14:14:49 +0800

    删除不必要的文件

--- library/imports/11/114bf8cd-2539-4db2-9a7c-1e41e6980f1f.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-cc._RF.push(module, '114bfjNJTlNspp8HkHmmA8f', 'IUIBridge');
-// script/framework/ui/IUIBridge.ts
-
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-
-cc._RF.pop();
--- library/imports/9e/9e23c1a0-c40e-40c9-bc72-6c012b6bb6af.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-cc._RF.push(module, '9e23cGgxA5AybxybAEra7av', 'IFSM');
-// script/framework/fsm/IFSM.ts
-
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-
-cc._RF.pop();
